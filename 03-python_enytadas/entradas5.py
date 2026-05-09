a6eb93d7068c40ca66dbd5948d82d4949fd28e82
rayssaah2007-spec <rayssaah2007@gmail.com>
2026-05-09 11:38:16 -0300

    Add files via upload

---
+++ 03-python_enytadas/entradas5.py
@@ -0,0 +1,16 @@
+# autor: Rayssa Ramos 
+#projetos: entradas f-strig
+
+valor1 =int(input('Digite o primeiro valor: '))
+valor2 =int(input('Digite o segundo valor: '))
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+
+# exibindo os resultados 
+print(f'O resultado da soma é: {soma}')
+print(f'O resultado da subtração é: {subtracao}')
+print(f'O resultado da multiplicação é: {multiplicacao}')
+print(f'O resultado da divisão é: {divisao}')
---
+++ 03-python_enytadas/entradas6.py
@@ -0,0 +1,8 @@
+# autor: Rayssa Ramos 
+# projeto: imc.py
+
+peso =float(input("Digite seu peso: "))
+altura =float(input("Digite sua altura: "))
+
+imc = peso / (altura * altura)
+print(f"Seu imc é:{imc}")
