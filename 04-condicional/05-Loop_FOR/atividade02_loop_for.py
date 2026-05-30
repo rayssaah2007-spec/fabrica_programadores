feb13c494ba088eb9fadf38e8c9c7555394a2efb
rayssaah2007-spec <rayssaah2007@gmail.com>
2026-05-30 10:20:30 -0300

    Delete 04-condicional

--- 04-condicional/05-Loop_FOR/atividade02_loop_for.py
+++
@@ -1,7 +0,0 @@
-#Autor: Rayssa Ramoas
-# Projeto: Tabuada com loop For
-
-numero = 10
-#loop For
-for i in range (1,11):
-    print(f'{numero} x {i} = {i * numero}')
--- 04-condicional/05-Loop_FOR/atividade03_loop_for.py
+++
@@ -1,8 +0,0 @@
-#Autor: Rayssa Ramos
-#Projeto: Tabuada com loop for
-
-numero = int(input("Digite o numero: "))
-
-#loop For
-for i in range (1,11):
-    print(f'{numero} x {i} = {i * numero}')
