0fc8abb182566a45a5aa223da2839b12ef39ab41
rayssaah2007-spec <rayssaah2007@gmail.com>
2026-05-30 11:45:12 -0300

    Add files via upload

---
+++ 06-Loop_while/atividade01_loop_while.py
@@ -0,0 +1,10 @@
+#Autor: Rayssa Ramos
+#Projeto:Loop while
+numero = 2
+i = 1
+
+while i <=10:
+    print(f"valor de i antes da interação{i}")
+    print(f"{numero} x {i} = {numero * i}")
+    i = i + 1  
+    print(f"valor do i depois da interação {i}")
---
+++ 06-Loop_while/atividade02_loop_while.py
@@ -0,0 +1,8 @@
+#Autor: Rayssa Ramos
+ #Projeto: Loop while
+ numero = 2
+ i = 1
+
+ while i <= 10:
+    print(f"{numero} x {i} = {numero * 1}")
+    i = i + 1
